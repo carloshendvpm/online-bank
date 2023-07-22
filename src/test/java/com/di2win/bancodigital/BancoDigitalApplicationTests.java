511c71d25ee24883193e674bab91d3a0fefb3b96
Carlos Henrique <hencarlosdv@gmail.com>
2023-07-22 13:08:54 -0300

    test: initial tests

---
+++ src/test/java/com/di2win/bancodigital/BancoDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.di2win.bancodigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
