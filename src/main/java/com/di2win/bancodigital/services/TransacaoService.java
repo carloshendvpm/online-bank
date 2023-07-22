131f39780120974dc7ca1d2f192471915fe8f6ac
Carlos Henrique <hencarlosdv@gmail.com>
2023-07-22 15:42:57 -0300

    feat: add transaction for extratos

--- src/main/java/com/di2win/bancodigital/services/TransacaoService.java
+++ src/main/java/com/di2win/bancodigital/services/TransacaoService.java
@@ -1,21 +1,39 @@
 package com.di2win.bancodigital.services;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.di2win.bancodigital.TipoTransacao;
 import com.di2win.bancodigital.models.Transacao;
 import com.di2win.bancodigital.repositories.TransacaoRepository;
 
 @Service
 public class TransacaoService {
 
-    @Autowired
-    private TransacaoRepository transacaoRepository;
+  @Autowired
+  private TransacaoRepository transacaoRepository;
 
-    public List<Transacao> getExtrato(Long contaId, LocalDate inicio, LocalDate fim) {
-      return transacaoRepository.findByContaIdAndDataHoraBetween(contaId, inicio, fim);
-    }
+  public List<Transacao> getExtrato(Long contaId) {
+      return transacaoRepository.findByContaId(contaId);
+  }
+
+  public List<Transacao> getExtratoPorPeriodo(Long contaId, LocalDate inicio, LocalDate fim) {
+    LocalDateTime inicioDoDia = inicio.atStartOfDay();
+    LocalDateTime fimDoDia = fim.atTime(23, 59, 59);
+    return transacaoRepository.findByContaIdAndDataHoraBetween(contaId, inicioDoDia, fimDoDia);
+}
+  public BigDecimal getTotalSaquesDoDia(Long contaId) {
+      LocalDateTime inicioDoDia = LocalDate.now().atStartOfDay();
+      LocalDateTime fimDoDia = LocalDate.now().atTime(LocalTime.MAX);
+      List<Transacao> saquesDoDia = transacaoRepository.findByContaIdAndTipoAndDataHoraBetween(contaId, TipoTransacao.SAQUE, inicioDoDia, fimDoDia);
+      return saquesDoDia.stream()
+          .map(Transacao::getValor)
+          .reduce(BigDecimal.ZERO, BigDecimal::add);
+  }
 }
