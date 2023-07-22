540e99290059e31469ecdc24636e4fe79e17fafa
Carlos Henrique <hencarlosdv@gmail.com>
2023-07-22 15:39:19 -0300

    feat: add daily limit

--- src/main/java/com/di2win/bancodigital/dtos/ContaDTO.java
+++ src/main/java/com/di2win/bancodigital/dtos/ContaDTO.java
@@ -1,8 +1,11 @@
 package com.di2win.bancodigital.dtos;
 
+import java.math.BigDecimal;
+
 import lombok.Data;
 
 @Data
 public class ContaDTO {
   private String cpf;
+  private BigDecimal limiteDiario;
 }
