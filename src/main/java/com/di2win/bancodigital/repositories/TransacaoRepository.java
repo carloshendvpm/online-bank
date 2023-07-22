316d1e05a36e52cf735e86ba632e226c15ae245f
Carlos Henrique <hencarlosdv@gmail.com>
2023-07-22 15:42:12 -0300

    feat: create methods for find Accounts

--- src/main/java/com/di2win/bancodigital/repositories/TransacaoRepository.java
+++ src/main/java/com/di2win/bancodigital/repositories/TransacaoRepository.java
@@ -1,12 +1,15 @@
 package com.di2win.bancodigital.repositories;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.di2win.bancodigital.TipoTransacao;
 import com.di2win.bancodigital.models.Transacao;
 
 public interface TransacaoRepository extends JpaRepository<Transacao, Long> {
-    List<Transacao> findByContaIdAndDataHoraBetween(Long contaId, LocalDate inicio, LocalDate fim);
+  List<Transacao> findByContaIdAndDataHoraBetween(Long contaId, LocalDateTime inicio, LocalDateTime fim);
+  List<Transacao> findByContaIdAndTipoAndDataHoraBetween(Long contaId, TipoTransacao tipo, LocalDateTime inicio, LocalDateTime fim);
+  List<Transacao> findByContaId(Long contaId);
 }
