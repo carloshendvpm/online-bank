085faec8f05f3df18c3fe7cd4adfded798326aa1
Carlos Henrique <hencarlosdv@gmail.com>
2023-07-22 18:05:09 -0300

    refactor: using builder

--- src/main/java/com/di2win/bancodigital/model/Conta.java
+++ src/main/java/com/di2win/bancodigital/model/Conta.java
@@ -11,42 +11,35 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor
+@AllArgsConstructor
 @Data
+@Builder
 public class Conta {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
 
-  @Column(nullable = false, unique = true)
+  @Column(nullable = false, unique = true, name = "numero_agencia")
   private String numero;
 
-  @Column(nullable = false, unique = true)
+  @Column(nullable = false, length = 4, name = "agencia")
   private String agencia;
+  @Column(name = "saldo")
   private BigDecimal saldo;
+  @Column(nullable = false, name = "bloqueada")
   private boolean bloqueada;
-  @Column(nullable = false)
+  @Column(nullable = false, name = "limite_diario")
   private BigDecimal limiteDiario;
 
   @ManyToOne
   @JoinColumn(name = "cliente_id")
   @JsonBackReference
   private Cliente cliente;
-
-  public Conta(String numero, String agencia, Cliente cliente, BigDecimal limiteDiario) {
-    this.numero = numero;
-    this.agencia = agencia;
-    this.cliente = cliente;
-    this.saldo = BigDecimal.ZERO;
-    this.bloqueada = false;
-    if (limiteDiario == null) {
-      this.limiteDiario = new BigDecimal(5000);
-    } else {
-        this.limiteDiario = limiteDiario;
-    }
-}
 }
--- src/main/java/com/di2win/bancodigital/model/Transacao.java
+++ src/main/java/com/di2win/bancodigital/model/Transacao.java
@@ -13,10 +13,16 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Transacao {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
