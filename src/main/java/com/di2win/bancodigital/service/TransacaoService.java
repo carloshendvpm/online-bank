4df5a3950f96509866b967bb107b458f1c719f1b
Carlos Henrique <hencarlosdv@gmail.com>
2023-07-23 06:45:55 -0300

    feat: create validation for account blocked

--- src/main/java/com/di2win/bancodigital/service/TransacaoService.java
+++ src/main/java/com/di2win/bancodigital/service/TransacaoService.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.di2win.bancodigital.enums.TipoTransacao;
+import com.di2win.bancodigital.model.Conta;
 import com.di2win.bancodigital.model.Transacao;
 import com.di2win.bancodigital.repository.TransacaoRepository;
 
@@ -19,16 +20,30 @@ public class TransacaoService implements ITransacaoService {
   @Autowired
   private TransacaoRepository transacaoRepository;
 
+  @Autowired
+  private ContaService contaService;
+
+  private void validarConta(Long contaId) {
+    Conta conta = contaService.find(contaId);
+    if (conta.isBloqueada()) {
+      throw new IllegalArgumentException("A conta está bloqueada e não pode consultar o extrato.");
+    }
+  }
+
+
   public List<Transacao> getExtrato(Long contaId) {
+      validarConta(contaId);
       return transacaoRepository.findByContaId(contaId);
   }
 
   public List<Transacao> getExtratoPorPeriodo(Long contaId, LocalDate inicio, LocalDate fim) {
+    validarConta(contaId);
     LocalDateTime inicioDoDia = inicio.atStartOfDay();
     LocalDateTime fimDoDia = fim.atTime(23, 59, 59);
     return transacaoRepository.findByContaIdAndDataHoraBetween(contaId, inicioDoDia, fimDoDia);
 }
   public BigDecimal getTotalSaquesDoDia(Long contaId) {
+      validarConta(contaId);
       LocalDateTime inicioDoDia = LocalDate.now().atStartOfDay();
       LocalDateTime fimDoDia = LocalDate.now().atTime(LocalTime.MAX);
       List<Transacao> saquesDoDia = transacaoRepository.findByContaIdAndTipoAndDataHoraBetween(contaId, TipoTransacao.SAQUE, inicioDoDia, fimDoDia);
